aed895797ac55b703eaece3b74018b4464d03436
rremigio <42788999+rremigio@users.noreply.github.com>
2025-12-08 07:58:43 +0000

    Deploying to gh-pages from @ rremigio/rremigio.github.io@e976325b9be18ac91bc7f552e18e805ede9da40f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,61 +39,16 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-searching-for-extended-ly-alpha-around-high-redshift-quasars",
+          section: "News",},{id: "projects-searching-for-extended-ly-alpha-around-high-redshift-quasars",
           title: 'Searching for Extended Ly-alpha around High Redshift Quasars',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/lya_kin_project/";
+              window.location.href = "/projects/lya_kin/";
             },},{id: "projects-spatially-resolved-o-iii-kinematics-of-nearby-active-galaxies",
           title: 'Spatially Resolved [O III] Kinematics of Nearby Active Galaxies',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/oiii_kin_project/";
+              window.location.href = "/projects/oiii_kin/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
