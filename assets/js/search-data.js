946138b641256c5b052ea15e2e7c165f069e5cc2
rremigio <42788999+rremigio@users.noreply.github.com>
2025-12-08 04:26:19 +0000

    Deploying to gh-pages from @ rremigio/rremigio.github.io@5d1507bed2c31047d21cbc1ba6f990bdb230a29d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-spatially-resolved-o-iii-kinematics-of-nearby-agn-hosts",
+          title: 'Spatially Resolved [O III] Kinematics of Nearby AGN Hosts',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
