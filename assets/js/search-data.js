d16724bc0a1151fc4ac913afc901ccf87f1b00fe
rremigio <42788999+rremigio@users.noreply.github.com>
2025-12-08 04:33:24 +0000

    Deploying to gh-pages from @ rremigio/rremigio.github.io@084842c7fb6d10bfcc6cdf62edbc3fabf5827edc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,12 +39,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-spatially-resolved-o-iii-kinematics-of-nearby-agn-hosts",
-          title: 'Spatially Resolved [O III] Kinematics of Nearby AGN Hosts',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/10_project/";
-            },},{id: "projects-project-1",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
@@ -89,6 +84,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-spatially-resolved-o-iii-kinematics-of-nearby-agn-hosts",
+          title: 'Spatially Resolved [O III] Kinematics of Nearby AGN Hosts',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/oiii_kin/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
